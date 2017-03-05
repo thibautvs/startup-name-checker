fe817453a04c4ff57bc5cdfb957f293e301ed957
Thibaut Van Spaandonck <thibaut@urge2code.com>
2017-03-05 17:29:04 +0100

    Implemented dedicated module for checkers

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-import http from 'http'
 import chalk from 'chalk'
-import config from './config.json'
-
-const log = console.log
+import check from './checker'
 
 if (process.argv.length !== 3) {
-  log(chalk.red('Wrong number of arguments. Expected 1 argument: name.'))
+  console.log(chalk.red('Wrong number of arguments. Expected 1 argument: name.'))
   process.exit(1)
 }
 
-checkDomainNames()
-checkSocialMediaHandles()
-
-function checkDomainNames() {
-  log(chalk.underline.bold('Domain names'))
-  const name = process.argv[2]
-  log(config.domains)
-}
-
-function checkSocialMediaHandles() {
-  log(chalk.underline.bold('Social media handles'))
-  log(config.social)
-}
+const name = process.argv[2].replace(/\s/g, '')
+check(name)
+console.log(chalk.green('\nDone!'))
